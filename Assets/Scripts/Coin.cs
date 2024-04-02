9dff06a174aeb33b99352fc4f6a1ff6ea3cbd49c
alangamu <alangamu@gmail.com>
2024-04-02 20:20:30 -0300

    coin collectable

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,13 @@
+﻿using AlbertoGarrido.Platformer.Intefaces;
+using UnityEngine;
+
+namespace AlbertoGarrido.Platformer
+{
+    public class Coin : MonoBehaviour, ICollectable
+    {
+        public void Collect()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/GameEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.ScriptableObjects
+{
+    [CreateAssetMenu]
+    public class GameEvent : ScriptableObject
+    {
+        public event Action OnRaise;
+
+        public void Raise()
+        {
+            OnRaise?.Invoke();
+        }
+    }
+}
